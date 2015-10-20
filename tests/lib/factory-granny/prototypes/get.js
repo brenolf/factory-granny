f39d1e326504df7269c88a89204fc8d897002e71
Breno Lima de Freitas <brenolimadefreitas@gmail.com>
2015-10-20 12:46:22 -0200

    Get always returns a function

--- tests/lib/factory-granny/prototypes/get.js
+++ tests/lib/factory-granny/prototypes/get.js
@@ -12,29 +12,17 @@ describe('FactoryGranny#get', function () {
     subject = FactoryGranny.prototype.get
   })
 
-  context('when there is no static properties', function () {
-    it('returns a common object', function () {
-      var result = subject.call(FactoryGranny)
+  it('returns a constructor for the object with static properties',
+  function () {
+    var result = subject.call(FactoryGrannyStatic)
 
-      expect(result).to.eql({
-        x: 42
-      })
-    })
-  })
+    var expected = {
+      x: 42
+    }
 
-  context('when there is static properties', function () {
-    it('returns a constructor for the object with static properties',
-    function () {
-      var result = subject.call(FactoryGrannyStatic)
-
-      var expected = {
-        x: 42
-      }
-
-      expect(result._instance).to.eql(expected)
-      expect(result).to.be.a.function
-      expect(result()).to.eql(expected)
-      expect(result.attr1).to.eql(42)
-    })
+    expect(result._instance).to.eql(expected)
+    expect(result).to.be.a.function
+    expect(result()).to.eql(expected)
+    expect(result.attr1).to.eql(42)
   })
 })
